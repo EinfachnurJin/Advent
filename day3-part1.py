4d530d140a4a5c48073021b0b4e3860c326cd683
EinfachnurJin <einfachnurjin@gmail.com>
2024-12-20 15:34:02 +0000

    day3-part1

--- day3-part1.py
+++ day3-part1.py
@@ -1,21 +1,22 @@
 import re
 
 def digiMule(input, wort):
-    mus = r"mul\(\d{1,3},(\d{1,3})\)"
+    mus = r"mul\((\d{1,3}),(\d{1,3})\)"
     such = re.findall(mus, input)
     if such:
         return such#.group()
     return None
 
-input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+input = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
 
-print(digiMule(input, "mul"))
+#print(digiMule(input, "mul"))
 multi = digiMule(input, "mul")
-summe = 1
+summe = 0
 for i in multi:
-    (a,b) = map(int,i)
-    summe += a*b
-        
+    
+   (a,b) = map(int,i)
+   summe += a*b
+
     
 
 
